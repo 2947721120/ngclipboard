e7ba535b136e73cf1c859a6fd0cca9968965e0ef
炎木 <2947721120@users.noreply.github.com>
2016-09-30 00:45:26 +0800

    Update snippets.js

--- assets/scripts/snippets.js
+++ assets/scripts/snippets.js
@@ -1,7 +1,7 @@
 var snippets = document.querySelectorAll('.snippet');
 
 [].forEach.call(snippets, function(snippet) {
-    snippet.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="assets/images/clippy.svg" alt="Copy to clipboard"></button>');
+    snippet.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="assets/images/clippy.svg" alt="复制到剪贴板"></button>');
 });
 
 var clipboardSnippets = new Clipboard('[data-clipboard-snippet]', {
@@ -13,7 +13,7 @@ var clipboardSnippets = new Clipboard('[data-clipboard-snippet]', {
 clipboardSnippets.on('success', function(e) {
     e.clearSelection();
 
-    showTooltip(e.trigger, 'Copied!');
+    showTooltip(e.trigger, '已复制!');
 });
 
 clipboardSnippets.on('error', function(e) {
