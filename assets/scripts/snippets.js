a1328f9fc0fd2065a861f1d23d330d13ea959b55
炎木 <2947721120@users.noreply.github.com>
2016-09-30 02:28:55 +0800

    Update snippets.js

--- assets/scripts/snippets.js
+++ assets/scripts/snippets.js
@@ -1,7 +1,7 @@
 var snippets = document.querySelectorAll('.snippet');
 
 [].forEach.call(snippets, function(snippet) {
-    snippet.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="assets/images/clippy.svg" alt="复制到剪贴板"></button>');
+    snippet.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="https://2947721120.github.io/ngclipboard/assets/images/clippy.svg" alt="复制到剪贴板"></button>');
 });
 
 var clipboardSnippets = new Clipboard('[data-clipboard-snippet]', {
